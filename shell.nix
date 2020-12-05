54509729b76ff62dd5076c07500ed4762a85abac
Thomaz Leite <1@thomazleite.com>
2020-12-04 22:28:26 -0300

    Initialize repo

---
+++ shell.nix
@@ -0,0 +1,47 @@
+{ pkgs ? import ./pkgs.nix { } }:
+let
+  kubebuilder = pkgs.buildGo115Module rec {
+    pname = "kubebuilder";
+    version = "2.3.1";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "kubernetes-sigs";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "07frc9kl6rlrz2hjm72z9i12inn22jqykzzhlhf9mcr9fv21s3gk";
+    };
+
+    vendorSha256 = "079cnaflk2ap5fb357151fdqk7wr37dpghd3lmrmhcqwpfwp022m";
+
+    subPackages = [ "./cmd" ];
+
+    postInstall = ''
+      mv $out/bin/cmd $out/bin/kubebuilder
+    '';
+
+    doCheck = false;
+  };
+in
+(pkgs.callPackage ./default.nix { }).overrideAttrs (
+  attrs: {
+    src = null;
+
+    nativeBuildInputs = [
+      kubebuilder
+      pkgs.go_1_15
+      pkgs.kind
+      pkgs.kubectl
+      pkgs.kustomize
+    ];
+
+    shellHook = ''
+      export GOPATH="$(pwd)/.go"
+      export GOCACHE=""
+      export GO111MODULE="on"
+
+      go mod init ${attrs.goPackagePath}
+
+      set +v
+    '';
+  }
+)
