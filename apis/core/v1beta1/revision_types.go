e262bb2817278d10ead624c0b39ed13678d0ef28
Thomaz Leite <1@thomazleite.com>
2021-01-01 18:37:52 -0300

    Update revision state when job is finished

--- apis/core/v1beta1/revision_types.go
+++ apis/core/v1beta1/revision_types.go
@@ -9,7 +9,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// +kubebuilder:validation:Enum=Pending;Building;Ready;Failed
+// +kubebuilder:validation:Enum=Pending;Failed;Succeeded
 
 type State string
 
--- controllers/core/revision_controller.go
+++ controllers/core/revision_controller.go
@@ -6,9 +6,15 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/go-logr/logr"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -22,39 +28,57 @@ type RevisionReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+type contextKey string
+
+var (
+	contextKeyLog      = contextKey("log")
+	contextKeyRequest  = contextKey("request")
+	contextKeyRevision = contextKey("revision")
+)
+
 // +kubebuilder:rbac:groups=core.hedron.build,resources=revisions,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core.hedron.build,resources=revisions/status,verbs=get;update;patch
 
-func (r *RevisionReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	ctx := context.Background()
-	log := r.Log.WithValues("revision", req.NamespacedName)
+func (r *RevisionReconciler) Reconcile(request ctrl.Request) (ctrl.Result, error) {
+	log := r.Log.WithValues("revision", request.NamespacedName)
+	ctx := context.WithValue(context.Background(), contextKeyLog, log)
 
-	var revision v1beta1.Revision
+	requestCtx := context.WithValue(ctx, contextKeyRequest, request)
 
-	// Fetch Revision resource
-	err := r.Get(ctx, req.NamespacedName, &revision)
+	revision, err := r.fetchRevision(requestCtx)
 	if err != nil {
-		log.Info("Failed to fetch revision")
+		log.Error(err, "Failed to fetch revision")
 
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	switch revision.Status.State {
-	case "Pending":
-		return r.ReconcilePending(log, revision)
-	case "Building":
-		log.Info("Revision is building")
-	case "Ready":
-		log.Info("Revision is ready")
-	case "Failed":
-		log.Info("Revision is failed")
+	revisionCtx := context.WithValue(requestCtx, contextKeyRevision, revision)
+
+	if revision.Status.State == "Failed" || revision.Status.State == "Succeeded" {
+		log.Info(fmt.Sprintf("Revision is %s", strings.ToLower(string(revision.Status.State))))
 	}
 
-	return ctrl.Result{}, nil
-}
+	job, err := r.fetchJob(revisionCtx)
+	if err != nil {
+		job, err = r.createJob(revisionCtx)
+		if err != nil {
+			log.Error(err, "Failed to create job")
+		}
+	}
 
-func (r *RevisionReconciler) ReconcilePending(log logr.Logger, revision v1beta1.Revision) (ctrl.Result, error) {
-	log.Info("Revision is pending")
+	if job.Status.Active == 1 {
+		revision.Status.State = "Pending"
+	}
+	if job.Status.Failed == 1 {
+		revision.Status.State = "Failed"
+	}
+	if job.Status.Succeeded == 1 {
+		revision.Status.State = "Succeeded"
+	}
+
+	if err = r.Update(ctx, &revision); err != nil {
+		log.Error(err, "Failed to update revision state")
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -63,4 +87,50 @@ func (r *RevisionReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1beta1.Revision{}).
 		Complete(r)
+}
+
+func (r *RevisionReconciler) createJob(ctx context.Context) (batchv1.Job, error) {
+	revision := ctx.Value(contextKeyRevision).(v1beta1.Revision)
+
+	job := batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-job", revision.Name),
+			Namespace: revision.Namespace,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:    "hedron-runner",
+							Image:   "ghcr.io/thmzlt/hedron-runner:latest",
+							Command: []string{"sleep", "30"},
+						},
+					},
+					RestartPolicy: "Never",
+				},
+			},
+		},
+	}
+
+	return job, r.Create(ctx, &job)
+}
+
+func (r *RevisionReconciler) fetchJob(ctx context.Context) (batchv1.Job, error) {
+	var job batchv1.Job
+
+	request := ctx.Value(contextKeyRequest).(ctrl.Request)
+	jobName := types.NamespacedName{
+		Namespace: request.NamespacedName.Namespace,
+		Name:      fmt.Sprintf("%s-job", request.NamespacedName.Name),
+	}
+
+	return job, r.Get(ctx, jobName, &job)
+}
+
+func (r *RevisionReconciler) fetchRevision(ctx context.Context) (v1beta1.Revision, error) {
+	var revision v1beta1.Revision
+
+	request := ctx.Value(contextKeyRequest).(ctrl.Request)
+	return revision, r.Get(ctx, request.NamespacedName, &revision)
 }
